ebc615b458d9ad118089bac1afec97661b38e224
zwj7181 <45657181@qq.com>
2020-05-22 20:06:12 +0800

    feat：add antd-mobile qrcode.react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
+
+import { DatePicker, List, Button, InputItem, WhiteSpace } from 'antd-mobile';
+import { createForm } from 'rc-form';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const QRCode = require('qrcode.react');
+
+function App({ form }) {
+  const { getFieldProps } = form;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        qrcode form
       </header>
+      <section>
+        <form>
+          <List renderHeader={() => 'Customize to focus'}>
+            <InputItem
+              {...getFieldProps('autofocus')}
+              clear
+              placeholder="auto focus"
+            >标题</InputItem>
+            <InputItem
+              {...getFieldProps('focus')}
+              clear
+              placeholder="click the button below to focus"
+            >标题</InputItem>
+            <List.Item>
+              <div
+                style={{ width: '100%', color: '#108ee9', textAlign: 'center' }}
+              >
+                click to focus
+              </div>
+            </List.Item>
+            <Button type="primary">primary</Button><WhiteSpace />
+          </List>
+        </form>
+      </section>
+      <footer>
+        <QRCode value="http://facebook.github.io/react/" />
+      </footer>
     </div>
   );
 }
 
-export default App;
+export default createForm()(App);
