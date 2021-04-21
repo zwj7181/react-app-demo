0ca84c769ab40053662b2706a4c010812005713e
zwj7181 <45657181@qq.com>
2021-04-21 12:10:48 +0800

    fix: 路由改HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
 import Index from './pages/index'
 import Test from './pages/test'
@@ -9,7 +9,7 @@ export default class App extends Component {
     return (
       <Router>
         <Route exact path="/" component={Index} />
-        <Route exact path="/test" component={Test} />
+        <Route path="/test" component={Test} />
       </Router>
     )
   }
